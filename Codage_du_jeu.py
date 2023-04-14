b649d716b98eefc09f9d161ecbcadca74805adaf
GridLock19 <119674852+GridLock19@users.noreply.github.com>
2023-04-14 14:49:05 +0200

    Update Codage_du_jeu.py

--- Codage_du_jeu.py
+++ Codage_du_jeu.py
@@ -1,68 +1,75 @@
 import pyxel, random, time
-"""Tu as pour mission de sauver la Terre d'une pluie de météorites en la guidant hors de danger en évitant chacune d'entre-elles"""
-
+"""Tu as pour mission de sauver la monkey d'une pluie de banana en la guidant hors de danger en évitant chacune d'entre-elles"""
 taille_fenetre=256
 pyxel.init(taille_fenetre, taille_fenetre, title="Astroide reign")
-pyxel.load("res (1).pyxres")
+pyxel.load("res2.pyxres")
 points2 = 0
 points = 0
 i = 1
 
 #coin haut gauche
-Terre_x = 60
-Terre_y = 60
+monkey_x = 60
+monkey_y = 60
 
-terre_width=16
-terre_height=16
+monkey_width=16
+monkey_height=16
+
+banana_height=16
+banana_width=16
+
+powerup_width=16
+powerup_height=16
 
 #30=1seconde
 Ticks = 100000
 Ticks2 = 100000 
-timer = 20
+timer = 30
 
-météorites_liste_up = []
+banana_liste_up = []
 
-météorites_liste_left = []
+banana_liste_left = []
+
+powerup_liste= []
 
 explosions_liste = []  
 
-def Terre_deplacement(x, y):
+def monkey_deplacement(x, y):
     """déplacement avec les touches de directions"""
     if timer > 20 :
         if pyxel.btn(pyxel.KEY_RIGHT):
-            if (x+terre_width< taille_fenetre) :
+            if (x+monkey_width < taille_fenetre) :
                 x = x + 2
         if pyxel.btn(pyxel.KEY_LEFT):
             if (x > 0) :
                 x = x - 2
         if pyxel.btn(pyxel.KEY_DOWN):
-            if (y+terre_height< taille_fenetre) :
+            if (y+monkey_height < taille_fenetre) :
                 y = y + 2
         if pyxel.btn(pyxel.KEY_UP):
             if (y > 0) :
                 y = y - 2
     if timer > 10 :
         if pyxel.btn(pyxel.KEY_RIGHT):
-            if (x+terre_width< taille_fenetre) :
+            if (x+monkey_width < taille_fenetre) :
                 x = x + 1.5
         if pyxel.btn(pyxel.KEY_LEFT):
             if (x > 0) :
                 x = x - 1.5
         if pyxel.btn(pyxel.KEY_DOWN):
-            if (y+terre_height< taille_fenetre) :
+            if (y+monkey_height < taille_fenetre) :
                 y = y + 1.5
         if pyxel.btn(pyxel.KEY_UP):
             if (y > 0) :
                 y = y - 1.5
     else :
         if pyxel.btn(pyxel.KEY_RIGHT):
-            if (x+terre_width< taille_fenetre) :
+            if (x+monkey_width < taille_fenetre) :
                 x = x + 1
         if pyxel.btn(pyxel.KEY_LEFT):
             if (x > 0) :
                 x = x - 1
         if pyxel.btn(pyxel.KEY_DOWN):
-            if (y+terre_height< taille_fenetre) :
+            if (y+monkey_height < taille_fenetre) :
                 y = y + 1
         if pyxel.btn(pyxel.KEY_UP):
             if (y > 0) :
@@ -70,60 +77,78 @@ def Terre_deplacement(x, y):
     return x, y
 
 
-def météorites_creation(météorites_liste_up, météorites_liste_left):
-    """création aléatoire des météorites"""
+def banana_et_powerup_creation(banana_liste_up, banana_liste_left, powerup_liste):
+    """création aléatoire des  et des powerup"""
 
-    # un météorite par seconde
+    # une banana par seconde
     if (pyxel.frame_count % 30 == 0):
-        météorites_liste_up.append([random.randint(0, taille_fenetre-8), 0])
-        météorites_liste_left.append([0,random.randint(0, taille_fenetre-8)])
+        banana_liste_up.append([random.randint(0, taille_fenetre-8), 0])
+        banana_liste_left.append([0,random.randint(0, taille_fenetre-8)])
+    # un powerup chaque 5 seconde
+    if timer < 20:
+        if (pyxel.frame_count % 150 == 0):
+            powerup_liste.append([random.randint(0, taille_fenetre-8), 0])
 
 
-def météorites_deplacement(météorites_liste_up, météorites_liste_left):
-    """déplacement des météorites vers le haut et suppression s'ils sortent du cadre"""
+
+def banana_et_powerup_deplacement(banana_liste_up, banana_liste_left):
+    """déplacement des banana vers le haut et suppression s'ils sortent du cadre"""
     if timer > 20 :
-        for météorite in météorites_liste_up:
-            météorite[1] += 2
-            if  météorite[1]>taille_fenetre:
-                météorites_liste_up.remove(météorite)
-        for météorite_left in météorites_liste_left:
-            météorite_left[0] += 2
-            if  météorite_left[0]>taille_fenetre:
-                météorites_liste_left.remove(météorite_left)
+        for banana in banana_liste_up:
+            banana[1] += 2
+            if  banana[1]>taille_fenetre:
+                banana_liste_up.remove(banana)
+        for banana_left in banana_liste_left:
+            banana_left[0] += 2
+            if  banana_left[0]>taille_fenetre:
+                banana_liste_left.remove(banana_left)
     else:
-        for météorite in météorites_liste_up:
-            météorite[1] += 4
-            if  météorite[1]>taille_fenetre:
-                météorites_liste_up.remove(météorite)
-        for météorite_left in météorites_liste_left:
-            météorite_left[0] += 4
-            if  météorite_left[0]>taille_fenetre:
-                météorites_liste_left.remove(météorite_left)
-        
+        for banana in banana_liste_up:
+            banana[1] += 4
+            if  banana[1]>taille_fenetre:
+                banana_liste_up.remove(banana)
+        for banana_left in banana_liste_left:
+            banana_left[0] += 4
+            if  banana_left[0]>taille_fenetre:
+                banana_liste_left.remove(banana_left)
+                
+    for powerup in powerup_liste:
+        powerup[0] += random.randint(-5,5)
+        if  powerup[0]>taille_fenetre:
+            powerup_liste.remove(powerup)
+    for powerup in powerup_liste:
+        powerup[1] += random.randint(0,3)
+        if  powerup[1]>taille_fenetre:
+            powerup_liste.remove(powerup)
 
-def Terre_suppression(Ticks, météorites_liste_up, météorites_liste_left):
-    """disparition du Terre et d'un météorite si contact"""
-    global points
+def monkey_suppression(Ticks, banana_liste_up, banana_liste_left):
+    """disparition du monkey et d'un banana si contact"""
+    global points, timer
 
-    for météorite in météorites_liste_up:
-        if météorite[0] <= Terre_x+terre_width and météorite[1] <= Terre_y+terre_height and météorite[0]+8 >= Terre_x and météorite[1]+8 >= Terre_y:
-            météorites_liste_up.remove(météorite)
+    for banana in banana_liste_up:
+        if banana[0] <= monkey_x+monkey_width and banana[1] <= monkey_y+monkey_height and banana[0]+8 >= monkey_x and banana[1]+8 >= monkey_y:
+            banana_liste_up.remove(banana)
             Ticks -= 1
             points += 1
-            explosions_creation(Terre_x, Terre_y)
-    for météorite in météorites_liste_left:
-        if météorite[0] <= Terre_x+terre_width and météorite[1] <= Terre_y+terre_height and météorite[0]+8 >= Terre_x and météorite[1]+8 >= Terre_y:
-            météorites_liste_left.remove(météorite)
+            explosions_creation(monkey_x, monkey_y)
+    for banana in banana_liste_left:
+        if banana[0] <= monkey_x+monkey_width and banana[1] <= monkey_y+monkey_height and banana[0]+8 >= monkey_x and banana[1]+8 >= monkey_y:
+            banana_liste_left.remove(banana)
             points += 1
         Ticks -= 1
-        explosions_creation(Terre_x, Terre_y)
+        explosions_creation(monkey_x, monkey_y)
+    if timer < 20:
+        for powerup in powerup_liste:
+            if powerup[0] <= monkey_x+monkey_width and powerup[1] <= monkey_y+monkey_height and powerup[0]+8 >= monkey_x and powerup[1]+8 >= monkey_y:
+                powerup_liste.remove(powerup)
+                timer += 5
     return Ticks
     
 def explosions_creation(x, y):
     """explosions aux points de collision entre deux objets"""
-    explosions_liste.append([x, y, 0])
-
-
+    explosions_liste.append([x, y, 1])
+    
+    
 
 def explosions_animation():
     """animation des explosions"""
@@ -132,10 +157,6 @@ def explosions_animation():
         if explosion[2] == 12:
             explosions_liste.remove(explosion)  
             
-def pointsystem():
-    global timer, points, points2
-    if timer == 1:
-        points2 = points
 
 
 def clock():
@@ -144,27 +165,28 @@ def clock():
     if Ticks2%30 == 0 :
         timer -= 1
         
-def endpoints():
-    global timer, points , points2
-    if timer > 1 :
-        points2 = points
-    return points
 
+def endpoints():
+    global timer,points,points2
+    if timer == 1 :
+        points2 = points
+    
+    
 
 def update():
     """mise à jour des variables (30 fois par seconde)"""
     
-    global Terre_x, Terre_y, météorites_liste_up, Ticks, explosions_liste, timer, Ticks2, points, i, points2
+    global monkey_x, monkey_y, banana_liste_up, Ticks, explosions_liste, timer, Ticks2, points, i, points2
 
-    Terre_x, Terre_y = Terre_deplacement(Terre_x, Terre_y)
+    monkey_x, monkey_y = monkey_deplacement(monkey_x, monkey_y)
 
-    météorites_creation(météorites_liste_up, météorites_liste_left)
-    météorites_deplacement(météorites_liste_up, météorites_liste_left)
+    banana_et_powerup_creation(banana_liste_up, banana_liste_left, powerup_liste)
+    banana_et_powerup_deplacement(banana_liste_up, banana_liste_left)
 
-    Ticks = Terre_suppression(Ticks, météorites_liste_up, météorites_liste_left)
+    Ticks = monkey_suppression(Ticks, banana_liste_up, banana_liste_left)
 
-    explosions_animation()
-
+    explosions_animation()   
+    
     endpoints()
 
     clock()
@@ -175,21 +197,35 @@ def draw():
     
     global Ticks2 , i
     pyxel.cls(0)
+      
+    if i < 16:
+        if Ticks2%7 == 0 :
+            i = i + 1
+            if i == 15:
+                i=1
+    pyxel.bltm(0,0,0,0,0,256,256)
+    if timer < 20 and timer > 0:
+        pyxel.text(90,8,"POWERUPS ARE ACTIVE",i)
     if timer > 0:
 
-        pyxel.text(5,5, 'Points: '+ str(points), 7)
+        pyxel.text(5,5, 'Points: '+ str(points), 4)
         
-        pyxel.text(taille_fenetre-30,5, 'Time:' + str(timer),7)
+        pyxel.text(taille_fenetre-30,5, 'Time:' + str(timer),4)
         
-        #pyxel.rect(Terre_x, Terre_y, 8, 8, 1)
-        pyxel.blt(Terre_x, Terre_y,0,48,112,terre_width,terre_height)
+        pyxel.blt(monkey_x, monkey_y,0,32,0,monkey_width,monkey_height,12)
 
-        for météorite in météorites_liste_up:
-            pyxel.rect(météorite[0], météorite[1], 8, 8, 8)
-        for météorite in météorites_liste_left:
-            pyxel.rect(météorite[0], météorite[1], 8, 8, 8)
+        for banana in banana_liste_up:
+            #pyxel.rect(banana[0], banana[1], 8, 8, 8)
+            pyxel.blt(banana[0], banana[1],0,0,0,banana_width,banana_height,12)
+        for banana in banana_liste_left:
+            #pyxel.rect(banana[0], banana[1], 8, 8, 8)
+            pyxel.blt(banana[0], banana[1],0,0,0,banana_width,banana_height,12)
+        for powerup in powerup_liste:
+            #pyxel.rect(powerup[0], powerup[1], 8, 8, 4)
+            pyxel.blt(powerup[0], powerup[1],0,16,0,powerup_width,powerup_height,12)
         for explosion in explosions_liste:
-            pyxel.circb(explosion[0]+4, explosion[1]+4, 2*(explosion[2]//4), 8+explosion[2]%3)            
+            pyxel.circb(explosion[0]+8, explosion[1]+16, 2*(explosion[2]//3),8)     
+                
 
     else:
         if i < 16:
@@ -198,6 +234,6 @@ def draw():
                 if i == 15:
                     i=1
             pyxel.text(110,110, 'GAME OVER', i)
-        pyxel.text(105,120, 'Vos Points:' + str(points),7)
+        pyxel.text(105,120, 'Vos Points:' + str(points2),4)
 
 pyxel.run(update, draw)
